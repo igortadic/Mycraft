990b87cf20a3619a284c73e9f3bca893c46e46ee
TehStud <igortadic.msc@gmail.com>
2022-09-20 21:53:46 +0200

    creating a block class "Voxel"

--- minecraft.py
+++ minecraft.py
@@ -1,40 +1,20 @@
 from ursina import *
 
-class Test_cube(Entity):
-	def __init__(self):
-		super().__init__(
-			model = 'cube',
-			color = color.white,
-			texture = 'white_cube',
-			rotation = Vec3(45, 45, 45)
-			)
-
-class Test_button(Button):
-	def __init__(self):
+class Voxel(Button):
+	def __init__(self, position = (0,0,0)):
 		super().__init__(
 			parent = scene,
-			model = 'cube',
-			texture = 'brick',
-			color = color.blue,
-			highlight_color = color.red,
-			pressed_color = color.lime)
-
-	def input(self, key):
-		if self.hovered:
-			if key == 'left mouse down':
-				print('button pressed')
-
-def update():
-	if held_keys['a']:
-		test_square.x -= 4 * time.dt
+			position = position,
+			model = 'cube', 
+			origin_y = 0.5,
+			texture = 'white_cube', 
+			color = color.white,
+			highlight_color = color.lime)
 
 app = Ursina()
 
-test_square = Entity(model = 'quad', color = color.red, scale = (1, 4), position = (5,1))
-
-sans_texture = load_texture('assets/Sans.png')
-sans = Entity(model = 'quad', texture = sans_texture)
-
-test_cube = Test_button()
+for z in range(8):
+	for x in range(8):
+		voxel = Voxel(position = (x,0,z))
 
 app.run()
