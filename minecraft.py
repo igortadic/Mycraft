164beeac0dc668c38ae1bcabd90238e81e3c5341
TehStud <igortadic.msc@gmail.com>
2022-09-20 19:53:40 +0200

    move to left with 'a' key

--- minecraft.py
+++ minecraft.py
@@ -1,1 +1,11 @@
-import ursina
+from ursina import *
+
+def update():
+	if held_keys['a']:
+		test_square.x -= 4 * time.dt
+
+app = Ursina()
+
+test_square = Entity(model = 'quad', color = color.red, scale = (1, 4), position = (5,1))
+
+app.run()
