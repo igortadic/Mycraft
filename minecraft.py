a09aa5d472c52d1c052a2e82a94b9616d513fade
TehStud <igortadic.msc@gmail.com>
2022-09-21 10:23:00 +0200

    adding grass texture

--- minecraft.py
+++ minecraft.py
@@ -1,20 +1,38 @@
 from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina() 
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture = load_texture('assets/dirt_block.png')
 
 class Voxel(Button):
-	def __init__(self, position = (0,0,0)):
+	def __init__(self, position = (0,0,0), texture = grass_texture):
 		super().__init__(
 			parent = scene,
 			position = position,
-			model = 'cube', 
+			model = 'assets/block', 
 			origin_y = 0.5,
-			texture = 'white_cube', 
-			color = color.white,
-			highlight_color = color.lime)
+			texture = texture, 
+			color = color.color(0,0,random.uniform(0.9,1)),
+			highlight_color = color.lime,
+			scale = 0.5)
 
-app = Ursina()
+	def input(self, key):
+		if self.hovered:
+			if key == 'left mouse down':
+				voxel = Voxel(position = self.position + mouse.normal)
 
-for z in range(8):
-	for x in range(8):
+			if key == 'right mouse down':
+				destroy(self)
+
+
+
+for z in range(20):
+	for x in range(20):
 		voxel = Voxel(position = (x,0,z))
+
+player = FirstPersonController()
 
 app.run()
